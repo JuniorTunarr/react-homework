018e36c7eb96fcf1b5f7fe98b2beee6d62683dc0
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-08-16 22:02:00 +0900

    🎉 리액트 2주차 과제 초기 세팅

---
+++ landing-page/src/components/GlobalNavigationBar.jsx
@@ -0,0 +1,15 @@
+const GlobalNavigationBar = () => {
+  return (
+    <ul>
+      <li>소개</li>
+      <li>과정 &gt;</li>
+      <li>연간 일정</li>
+      <li>학습 안내</li>
+      <li>FAQ</li>
+      <li>브랜드 이야기</li>
+      <li>정책 /환불 규정</li>
+    </ul>
+  );
+};
+
+export default GlobalNavigationBar;
